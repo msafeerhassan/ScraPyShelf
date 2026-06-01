f83aa26d0b59f2477323b6927dac35473b7115ac
msafeerhassan <msafeerhassan0@gmail.com>
2026-06-01 14:44:17 +0500

    project done

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # ScraPyShelf
+
+## Coded in Python
+
+### A basic-CLI based tool to scrap: [Website](https://books.toscrape.com/)
+
+#### Features:
+1. Number of Pages choosen by User
+2. Saving Results to CSV File
+3. Efficient Error Handling
+4. Multiple Pages Scrapper
+5. Well-structured and Clean Code
+6. Simple UI
+
+#### How to use it?
+1. Simply run the .exe (execution) file for running the scrapper.
+2. Enter the number of Pages to scrap (Minimum = 1 and Maximum = 50)
+3. Just wait and watch :)
+
+#### Screenshots:
--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from bs4 import BeautifulSoup
 import requests
 from urllib.parse import urljoin
+import csv, time
 
-url = "https://books.toscrape.com/"
+baseURL = "https://books.toscrape.com/"
+fileName = "results.csv"
+with open(fileName, mode='w', newline='', encoding="utf-8") as csvFile:
+    columnHeaders = ["Title", "Cost", "Availability", "Thumbnail Link", "Rating"]
+    writer = csv.writer(csvFile)
+    writer.writerow(columnHeaders)
 
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-
-def getBookTitles():
+def getBookTitles(soup):
     headings = soup.select('h3 a')
     titleList = []
     for title in headings:
@@ -15,7 +18,7 @@ def getBookTitles():
     
     return titleList
 
-def getBookCosts():
+def getBookCosts(soup):
     prices = soup.find_all(class_="price_color")
     priceList = []
     for price in prices:
@@ -24,21 +27,22 @@ def getBookCosts():
     for i in priceList:
         newPrice = i.replace("Â", "")
         newPriceList.append(newPrice)
+    
     return newPriceList
 
-def getStockAvailability():
+def getStockAvailability(soup):
     stockAvailability = soup.find_all(class_="instock availability")
     stockAvailabilityList = []
     for availability in stockAvailability:
         status = availability.get_text()
         if status.strip() == "In stock":
-            stockAvailabilityList.append(True)
+            stockAvailabilityList.append("Available")
         else:
-            stockAvailabilityList.append(False)
+            stockAvailabilityList.append("Unavailable")
     
-    print(stockAvailabilityList)
+    return stockAvailabilityList
 
-def getImageLink():
+def getImageLink(soup):
     imageLinks = soup.find_all(class_="thumbnail")
     imageLinksList = []
     for link in imageLinks:
@@ -47,9 +51,9 @@ def getImageLink():
             wholeUrl = urljoin("https://books.toscrape.com/", src)
             imageLinksList.append(wholeUrl)
     
-    print(imageLinksList)
+    return imageLinksList
 
-def getBookRating():
+def getBookRating(soup):
     bookRatings = soup.find_all('p', class_="star-rating")
     bookRatingsList = []
     for rating in bookRatings:
@@ -57,20 +61,81 @@ def getBookRating():
         for otherClass in allClasses:
             if otherClass != "star-rating":
                 if otherClass == 'One':
-                    string = '1/5'
+                    string = '1 out of 5'
                 elif otherClass == "Two":
-                    string = '2/5'
+                    string = '2 out of 5'
                 elif otherClass == "Three":
-                    string = "3/5"
+                    string = "3 out of 5"
                 elif otherClass == "Four":
-                    string = "4/5"
+                    string = "4 out of 5"
                 elif otherClass == "Five":
-                    string = "5/5"
+                    string = "5 out of 5"
                 else:
                     string = None
                 
                 bookRatingsList.append(string)
-    print(bookRatingsList)
+    
+    return bookRatingsList
+
+def saveResults(soup):
+    bookTitles = getBookTitles(soup)
+    bookCosts = getBookCosts(soup)
+    stockAvailability = getStockAvailability(soup)
+    thumbnailLinks = getImageLink(soup)
+    bookRatings = getBookRating(soup)
+
+    fileName = 'results.csv'
+
+    with open(fileName, mode='a', newline='', encoding="utf-8") as csvFile:
+        writer = csv.writer(csvFile)
+        for i in range(20):
+            writer.writerow([
+                bookTitles[i],
+                bookCosts[i],
+                stockAvailability[i],
+                thumbnailLinks[i],
+                bookRatings[i]
+            ])
 
 
-getBookRating()
+while True:
+    pagesRequired = int(input("Enter the number of Pages to Scrap (Maximum 50): "))
+    if pagesRequired >= 1 and pagesRequired <= 50:
+        break
+    else:
+        print("Please enter correct value!")
+        pass
+
+
+currentURL = "https://books.toscrape.com/catalogue/page-1.html"
+pageCount = 1
+
+while currentURL:
+    print(f"Scraping Page {pageCount}: {currentURL}")
+    try:
+        response = requests.get(currentURL)
+        if response.status_code != 200:
+            print(f"Failed to fetch page {pageCount}")
+            break
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        saveResults(soup)
+        if pageCount == pagesRequired:
+            print("Scrapping Done!")
+            break
+        else:
+            pass
+        
+        nextPage = soup.select_one('li.next a')
+
+        if nextPage and nextPage.has_attr('href'):
+            currentURL = urljoin(currentURL, nextPage['href'])
+            pageCount += 1
+        else:
+            print("Scrapping Complete!")
+            time.sleep(3)
+            currentURL = None
+    except Exception as e:
+        print(f"Error Occured on Page {pageCount}: {e}")
+        break
